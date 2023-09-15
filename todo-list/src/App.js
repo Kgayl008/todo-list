201ed9ef8228495af1f96d67ca7aa0e53548de0c
Kiyanna Gayle <Kgayl008@fiu>
2023-09-14 20:51:27 -0400

    create a Todo component

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,14 +1,24 @@
-import React from "react";
-import "./App.css"
+import React, {useState} from "react";
+import "./App.css";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 
 function App() {
-  return (
-    <div className="App">
-      <header>
-      <h1>To-do List</h1>
-      </header>
-    </div>
-  );
+    const [inputText, setInputText] = useState("");
+    // setInputText is out function that updates of variable. Pass it into the form as a props| Think of it as DATA
+    const [todos, setTodos] = useState([]);
+    return (
+        <div className="App">
+            <header>
+                <h1>To-do List</h1>
+            </header>
+            <Form inputText={inputText}
+                todos={todos}
+                setTodos={setTodos}
+                setInputText={setInputText}/>
+            <TodoList todos={todos}/>
+        </div>
+    );
 }
 
 export default App;
